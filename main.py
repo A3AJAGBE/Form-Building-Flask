62975e0550c2f2deedc4508c802f8e5ae95e560e
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-06 06:10:45 +0000

    Add form to login function

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Name')
+    password = StringField('Password')
+
 
 app = Flask(__name__)
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
+app.config['SECRET_KEY'] = SECRET_KEY
 
 
 @app.route('/')
@@ -8,9 +22,10 @@ def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=('GET', 'POST'))
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+    return render_template('login.html', form=form)
 
 
 if __name__ == '__main__':
